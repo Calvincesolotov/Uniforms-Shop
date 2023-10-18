5eff297a086eed95d2e5e0b4165afb66f65378a3
Calvince Solotov <solotovtechn@gmail.com>
2023-10-18 21:29:58 +0300

    created product.js ,deleted some main.html images

---
+++ scripts/product.js
@@ -0,0 +1,89 @@
+const products = [
+
+  {
+    id:
+    "e1d07eb678c6-e43638ce-6aa0-4b85-b27f",
+    image:"/Images/0.jpeg",
+    name:"Sigiria uniform",
+    rating: {
+      stars:4.5,
+      count:34
+    },
+    priceCents:2000,
+    keywords: [
+      "uniform",
+      "school",
+      "girl"
+    ]
+  },
+
+  {
+    id:
+    "896f-486349e85a3d-15b6fc6f-327a-4ec4",
+    image:"/Images/1.webp",
+    name:"omware uniform",
+    rating: {
+      stars:5,
+      count:77
+    },
+    priceCents:1500,
+    keywords: [
+      "uniform",
+      "school",
+      "girl"
+    ]
+  },
+
+  {
+    id:
+    "ea210004f2e-3d4ca15-0f35-48f5-b7a3",
+    image:"/Images/10.jpeg",
+    name:"Kanga",
+    rating: {
+      stars:4.5,
+      count:100
+    },
+    priceCents:2050,
+    keywords: [
+      "uniform",
+      "school",
+      "girl"
+    ]
+  },
+
+  {
+    id:
+    "8182611d9add-54e0eccd-8f36-462b-b68a",
+    image:"/Images/11.jpeg",
+    name:"Yago",
+    rating: {
+      stars:3,
+      count:45
+    },
+    priceCents:400,
+    keywords: [
+      "uniform",
+      "school",
+      "girl"
+    ]
+  },
+
+  {
+    id:
+    "8860-1f5a963e534b-3ebe75dc-64d2-4137",
+    image:"/Images/0.jpeg",
+    name:"Rapogi",
+    rating: {
+      stars:5,
+      count:44
+    },
+    priceCents:1000,
+    keywords: [
+      "uniform",
+      "school",
+      "girl"
+    ]
+  }
+
+
+];
